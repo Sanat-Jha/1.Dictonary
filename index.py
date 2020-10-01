53d60d8989e798f94c63e3f82886ffb90ba494d6
Sanat-Jha <69027091+Sanat-Jha@users.noreply.github.com>
2020-10-01 11:02:52 +0530

    Important comments done

--- index.py
+++ index.py
@@ -1,15 +1,15 @@
-from sys import version_info
-if version_info.major == 2:
+from sys import version_info       #importing sys 
+if version_info.major == 2:            #check version and import accordingly
     import Tkinter as tk
 elif version_info.major == 3:
     import tkinter as tk
     
 
     
-app = tk.Tk()
+app = tk.Tk()           #initiate
 labelExample = tk.Button(app, text="0")
 
-def change_label_number(num):
+def change_label_number(num):      #defining the function
     counter = int(str(labelExample['text']))
     counter += num
     labelExample.config(text=str(counter))
